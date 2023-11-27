ee01f48ffc7c8987c78f183d874801d0cefca716
Bismark Kadogbe <67119909+Nero-Gh@users.noreply.github.com>
2023-11-27 10:50:58 +0000

    Fix imports and add login functionality to header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, LoginComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  loggedDat: any;
 
+  constructor() {
+    //assigning the logged in user data to loggedData variable
+    const loggedData = localStorage.getItem('user');
+
+    if (loggedData) {
+      const data = JSON.parse(loggedData);
+      this.loggedDat = data;
+    }
+  }
+
+  logoff() {
+    localStorage.removeItem('user');
+    this.loggedDat = undefined;
+  }
 }
